deb41ab5ee82f2613a2997f4c66e17bdaaae831c
Jeremy Wood <farachan@gmail.com>
2018-04-19 23:48:16 -0400

    Export CheckboxGroupContext.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const { Provider, Consumer } = React.createContext();
+export const CheckboxGroupContext = React.createContext();
+
+const { Provider, Consumer } = CheckboxGroupContext;
 
 export const Checkbox = props => (
   <Consumer>
