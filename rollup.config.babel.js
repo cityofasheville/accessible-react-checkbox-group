2ecff5c6873f13b77d408e2648ff0e0dd5bd73e6
Jeremy Wood <farachan@gmail.com>
2018-05-21 14:24:47 -0400

    Prettierified rollup config.

--- rollup.config.babel.js
+++ rollup.config.babel.js
@@ -1,51 +1,51 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import postcss from 'rollup-plugin-postcss';
-import eslint from 'rollup-plugin-eslint';
-import clean from 'rollup-plugin-clean';
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import replace from "rollup-plugin-replace";
+import postcss from "rollup-plugin-postcss";
+import eslint from "rollup-plugin-eslint";
+import clean from "rollup-plugin-clean";
 
 export default {
-  input: './src/index.js',
+  input: "./src/index.js",
 
   output: [
     {
-      name: 'AccessibleReactCheckboxGroup',
-      sourceMap: 'true',
+      name: "AccessibleReactCheckboxGroup",
+      sourceMap: "true",
       globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
+        react: "React",
+        "react-dom": "ReactDOM"
       },
-      format: 'umd',
-      file: './lib/index.js',
+      format: "umd",
+      file: "./lib/index.js"
     },
     {
-      sourceMap: 'true',
+      sourceMap: "true",
       globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
+        react: "React",
+        "react-dom": "ReactDOM"
       },
-      format: 'es',
-      file: './lib/index.module.js',
-    },
+      format: "es",
+      file: "./lib/index.module.js"
+    }
   ],
 
   plugins: [
     clean(),
     eslint(),
     postcss({
-      modules: true,
+      modules: true
     }),
     babel({
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**"
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      "process.env.NODE_ENV": JSON.stringify("development")
     }),
     resolve(),
-    commonjs(),
+    commonjs()
   ],
 
-  external: ['react', 'react-dom'],
+  external: ["react", "react-dom"]
 };
